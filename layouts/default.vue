1db6091b11feeaff9528aa2120d7cbe012bb6cd6
Glenn Nytveit <glenn@admin.glasspaper-solutions.no>
2023-11-21 17:30:57 +0100

    #added standard layout

---
+++ layouts/default.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <!-- Header -->
+    <header>
+      <div class="header-content">
+        <h1>Your Website</h1>
+        <button @click="toggleMenu" class="burger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+
+    <!-- Navigation Burger Menu -->
+    <nav v-if="showMenu" class="burger-menu-nav">
+      <!-- Your navigation links go here -->
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </nav>
+
+    <!-- Main Content -->
+    <main>
+      <!-- Your main content goes here -->
+      <slot />
+    </main>
+
+    <!-- Footer -->
+    <footer>
+      <!-- Your footer content goes here -->
+      <p>&copy; 2023 Your Website. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+
+/* Responsive styles for the header */
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  text-align: center;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* Styles for the burger menu icon */
+.burger-menu {
+  display: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 10px;
+}
+
+.burger-menu span {
+  display: block;
+  height: 3px;
+  width: 25px;
+  background-color: #fff;
+  margin: 5px 0;
+}
+
+/* Responsive styles for the navigation burger menu */
+.burger-menu-nav {
+  display: flex;
+  flex-direction: column;
+  background-color: #333;
+  padding: 20px;
+  position: absolute;
+  top: 60px;
+  left: 0;
+  width: 100%;
+}
+
+.burger-menu-nav a {
+  color: #fff;
+  text-decoration: none;
+  margin-bottom: 10px;
+}
+
+/* Responsive styles for the main content */
+main {
+  padding: 20px;
+}
+
+/* Styles for the footer */
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  text-align: center;
+}
+
+@media (max-width: 768px) {
+  /* Show burger menu button on small screens */
+  .burger-menu {
+    display: block;
+  }
+
+  /* Hide the navigation menu by default on small screens */
+  .burger-menu-nav {
+    display: none;
+  }
+}
+</style>
